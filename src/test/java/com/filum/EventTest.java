3929e766f4db258841f35ee65ad6a6cdfae65b04
hiepnp-filum <hiep@filum.ai>
2021-10-28 17:38:08 +0700

    test: update test for float value handling

--- src/test/java/com/filum/EventTest.java
+++ src/test/java/com/filum/EventTest.java
@@ -2,53 +2,56 @@ package com.filum;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Iterator;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
+import com.filum.exception.FilumException;
 
 public class EventTest {
   @Test
   public void testCreateEventWithNullUserAndDeviceThrowsException() {
     assertThrows(
-        IllegalArgumentException.class,
+        FilumException.class,
         () -> {
-          new Event("test event", null, null);
+          new EventBuilder("test event");
         },
-        "Event must have one defined userId and/or deviceId");
+        "Event type must have one defined identify or track");
   }
 
   @Test
-  public void testToJsonObject() {
-    String eventType = "test event type";
+  public void testToJsonObject() throws FilumException {
+    String eventType = "identify";
     String userId = "test-user";
-    Event event = new Event(eventType, userId);
-    String msg = "Long string to be truncated.";
-    StringBuilder stringBuilder = new StringBuilder();
-    stringBuilder.append(msg);
-    for (int i = 0; i < 7; i++) {
-      stringBuilder.append(stringBuilder.toString());
-    }
-    String longStr = stringBuilder.toString();
-    assertEquals(3584, longStr.length());
+    EventBuilder builder = new EventBuilder(eventType);
+    builder.setUserID(userId);
     JSONObject eventProperties = new JSONObject();
-    eventProperties.put("event_type", eventType);
-    JSONArray eventMsgArray = new JSONArray();
-    eventMsgArray.put(longStr);
-    eventMsgArray.put(longStr);
-    eventProperties.put("event_message", eventMsgArray);
-    event.eventProperties = eventProperties;
+    eventProperties.put("float_type", 123.456f);
+    double doubleValue = 1234.567;
+    eventProperties.put("double_type", doubleValue);
+    builder.setEventParams(eventProperties);
+    Event event = builder.getEvent();
 
-    JSONObject truncatedEvent = event.toJsonObject();
-    JSONArray truncatedEventMsgArray =
-        truncatedEvent.getJSONObject("event_properties").getJSONArray("event_message");
-    for (int i = 0; i < truncatedEventMsgArray.length(); i++) {
-      String truncatedMsg = (String) truncatedEventMsgArray.get(i);
-      assertEquals(Constants.MAX_STRING_LENGTH, truncatedMsg.length());
+    JSONObject serialized = event.toJsonObject();
+    
+    assertEquals(eventType, serialized.getString("event_type"));
+    assertEquals(userId, serialized.getString("user_id"));
+    
+    JSONArray kvItems = serialized.getJSONArray("event_params");
+    for (int i = 0 ; i < kvItems.length(); i++) {
+        JSONObject item = (JSONObject) kvItems.get(i);
+        System.out.println(item);
+        JSONObject value = item.getJSONObject("value");
+        Iterator<String> keys = value.keys();
+        while(keys.hasNext()){
+            String key = keys.next();
+            assertTrue("double_value" == key);
+        }
     }
-    assertEquals(eventType, truncatedEvent.getString("event_type"));
-    assertEquals(userId, truncatedEvent.getString("user_id"));
-    assertEquals(
-        Constants.SDK_LIBRARY + "/" + Constants.SDK_VERSION, truncatedEvent.getString("library"));
+
+    // assertEquals(
+    //     Constants.SDK_LIBRARY + "/" + Constants.SDK_VERSION, serialized.getString("library"));
   }
 }
